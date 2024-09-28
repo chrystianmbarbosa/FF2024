db25304c154046e2d5821bfff258340af25b7279
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-09-27 22:18:18 -0300

    feat: Ex5 Corrigido

--- exercicios/ex5/js/script.js
+++ exercicios/ex5/js/script.js
@@ -2,3 +2,10 @@
 Crie 1 função que retone se o valor digitado 
 é ou não é um número e exiba em um alert.
 */
+
+function verificarNumero(valor) {
+    return isNaN(valor);
+}
+
+alert(`É uma letra? ${verificarNumero('L')}`);
+
